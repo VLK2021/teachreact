a9872817f55adcf9f1c699ec7a36a64c4ca00bee
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-01-27 03:47:44 +0200

    training useForm Joi

--- src/validators/form.validator.js
+++ src/validators/form.validator.js
@@ -2,7 +2,10 @@ import Joi from "joi";
 
 
 export const formValidator = Joi.object({
-    firstName: Joi.string().min(6).max(20).required(),
+    firstName: Joi.string().min(6).max(20).required().messages({
+        'string.empty': '"firstName" не може бути пустим',
+        'string.pattern.base': '"firstName" тільки букви мін 6 макс 20'
+    }),
     secondName: Joi.string().min(6).max(20).required(),
     phone: Joi.number().required(),
     email: Joi.string().min(10).max(40).required(),
