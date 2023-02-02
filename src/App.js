2d1dc3e59e817dbc8ff319425e6ddffdac977866
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-02-02 14:44:44 +0200

    training redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import Form from "./components/Form/Form";
-import Users from "./components/Users/Users";
 
 
 function App() {
@@ -8,8 +6,7 @@ function App() {
 
     return (
         <div className="App">
-            <Form/>
-            <Users/>
+
         </div>
     );
 }
