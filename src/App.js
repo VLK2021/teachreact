183d6ff7e2e1b5b658dd153dcf438f0d62ddcadb
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-02-05 21:08:49 +0200

    training useForm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Layout from "./Layout/Layout";
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
     return (
         <div className="App">
-            APP
+            <Layout/>
         </div>
     );
 }
---
+++ src/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from "../components/Form/Form";
+
+const Layout = () => {
+
+    return (
+        <div>
+            <Form/>
+        </div>
+    );
+};
+
+export default Layout;
