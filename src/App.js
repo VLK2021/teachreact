84feda341c45eca1e8541d9c7755c39c3b92eb62
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-09-19 21:43:28 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import {Route, Routes} from 'react-router-dom';
+import {Routes} from 'react-router-dom';
 
 import './App.css';
-import Layout from "./Layout/Layout";
 
 
 function App() {
@@ -10,9 +9,7 @@ function App() {
     return (
         <div className="App">
             <Routes>
-                <Route index element={<Layout/>}>
 
-                </Route>
             </Routes>
         </div>
     );
