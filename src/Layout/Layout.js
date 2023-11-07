733ba3b502712a97a3b5292be17e73883ce7bd51
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-11-07 23:36:29 +0200

    start project

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,40 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import './LayoutStyle.css';
 
 
 const Layout = () => {
-    const [description, setDescription] = useState('');
-    const [quantity, setQuantity] = useState(5);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newItem = {description, quantity, id: Date.now()};
-        console.log(newItem);
-    }
 
 
     return (
         <div className={'layout'}>
-            <form onSubmit={handleSubmit}>
-                <h2>Create select with numbers</h2>
-
-                <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-                    {Array.from({length: 20}, (_, i) => i + 1).map((num) => (
-                        <option value={num} key={num}>
-                            {num}
-                        </option>
-                    ))}
-                </select>
-
-                <input type="text"
-                       name={'text'}
-                       placeholder={'items...'}
-                       value={description}
-                       onChange={(e) => setDescription(e.target.value)}
-                />
-                <button>Add</button>
-            </form>
+            Layout
         </div>
     );
 };
