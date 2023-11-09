f6fb8254aa09a65722ca982ac47b586f95f5d75c
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-11-09 21:04:36 +0200

    flash card

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './LayoutStyle.css';
+import {questions} from "../constants/Questions";
 
 
 const Layout = () => {
+    const [contentId, setContentId] = useState(null);
+
+    const handleClick = (id) => {
+        setContentId(id !== contentId ? id : null);
+    }
 
 
     return (
-        <div className={'layout'}>
-            Layout
+        <div className={'layout flex-direction'}>
+            <h1>Questions</h1>
+
+            <div className={'questions-block flex'}>{
+                questions.length > 0 &&
+                questions.map(obj =>
+                    <div key={obj.id}
+                         className={`flex ${obj.id === contentId ? 'single-question-selected' : 'single-question'}`}
+                         onClick={() => handleClick(obj.id)}
+                    >
+                        {obj.id === contentId ? obj.answer : obj.question}
+                    </div>)
+            }</div>
         </div>
     );
 };
