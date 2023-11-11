58cf80205e01ca1631cbf0fdd5e0678670afe968
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-11-11 17:39:07 +0200

    start project

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import './LayoutStyle.css';
-import {data} from "../constants";
-import {Accordion} from "../conponents";
 
 
 const Layout = () => {
@@ -10,12 +8,8 @@ const Layout = () => {
 
 
     return (
-        <div className={'layout flex-direction'}>
-            {
-                data.map(obj => <Accordion key={obj.id}
-                                           obj={obj}
-                    />)
-            }
+        <div className={'layout'}>
+            Layout
         </div>
     );
 };
