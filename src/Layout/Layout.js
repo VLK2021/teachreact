428d15c4a59b8995f8ba71499aff954a5388b967
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-11-12 12:16:02 +0200

    bill

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,15 +1,71 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useForm} from "react-hook-form";
 
 import './LayoutStyle.css';
 
 
 const Layout = () => {
+    const {register, handleSubmit} = useForm();
 
+    const [current, setCurrent] = useState({});
+
+    const [allBill, setAllBill] = useState(0);
+    const [percent, setPercent] = useState(0);
+    const [result, setResult] = useState(0);
+
+
+    const submit = (data) => {
+        setCurrent(data)
+    }
+
+    useEffect(() => {
+        setAllBill(Number(current.bill));
+        const sum = (Number(allBill) / 100) * (Number(current.you) + (Number(current.friend))) / 2
+        setPercent(Math.ceil(sum));
+        setResult(allBill + percent);
+    }, [current, allBill, result, percent]);
 
 
     return (
         <div className={'layout'}>
-            Layout
+            <h1>bill</h1>
+
+            <form onChange={handleSubmit(submit)}>
+                <div>
+                    <label>How much was the bill?
+                        <input type="number" {...register('bill')}/>
+                    </label>
+                </div>
+
+                <div>
+                    <label>How did you like the service?
+                        <select {...register('you')}>
+                            <option value={0}>Dissatisfied(0%)</option>
+                            <option value={5}>It was okay(5%)</option>
+                            <option value={10}>It was good(10%)</option>
+                            <option value={20}>Absolutely amazing(20%)</option>
+                        </select>
+                    </label>
+                </div>
+
+                <div>
+                    <label>How did your friend like the service?
+                        <select {...register('friend')}>
+                            <option value={0}>Dissatisfied(0%)</option>
+                            <option value={5}>It was okay(5%)</option>
+                            <option value={10}>It was good(10%)</option>
+                            <option value={20}>Absolutely amazing(20%)</option>
+                        </select>
+                    </label>
+                </div>
+            </form>
+
+
+            {
+                <div className={'block-bill'}>
+                    <p>You pay ${result} (${allBill} + ${percent}tip) </p>
+                </div>
+            }
         </div>
     );
 };
