a991ceaaf720960245716e400e77009bf8378784
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-11-26 23:06:21 +0200

    start project

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,20 +1,13 @@
 import React from 'react';
 
 import './LayoutStyle.css';
-import {BoxForText} from "../components";
 
 
 const Layout = () => {
 
-
     return (
-        <main className={'layout flex-direction'}>
-            <h1>Show me all text</h1>
+        <main className={'layout'}>
 
-            <BoxForText>
-                usdg hggg loiu hjh bhjv vhgv vgyv vgyv vgyv cf cxdx xde zez zew zz z eszezesza
-                ghucbdasj b sdcd4vc dsbhb
-            </BoxForText>
         </main>
     );
 };
--- src/components/index.js
+++
@@ -1,1 +0,0 @@
-export * from './BoxForText/BoxForText';
