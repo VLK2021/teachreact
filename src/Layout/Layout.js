cd6c44989f6441be3aa72eaacd2bc4d508d4623c
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-11-28 23:53:24 +0200

    exchange

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './LayoutStyle.css';
 
 
 const Layout = () => {
+    const [amount, setAmount] = useState(1);
+    const [fromCur, setFromCur] = useState('EUR');
+    const [toCur, setToCur] = useState('USD');
+    const [converted, setConverted] = useState('');
+
+
+    useEffect(() => {
+        fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`)
+            .then(response => response.json())
+            .then(value => console.log(value))
+    }, [amount, fromCur, toCur]);
 
     return (
         <main className={'layout'}>
+            <input type="text" value={amount}
+                   onChange={(e) => setAmount(Number(e.target.value))}/>
 
+            <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="CAD">CAD</option>
+                <option value="INR">INR</option>
+            </select>
+
+            <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="CAD">CAD</option>
+                <option value="INR">INR</option>
+            </select>
+
+            <p>{converted} {toCur}</p>
         </main>
     );
 };
