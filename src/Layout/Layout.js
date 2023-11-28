b66a28a4200057176332d19c7173dd40d27cab98
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-11-29 00:32:00 +0200

    start

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,45 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
 import './LayoutStyle.css';
 
 
 const Layout = () => {
-    const [amount, setAmount] = useState(1);
-    const [fromCur, setFromCur] = useState('EUR');
-    const [toCur, setToCur] = useState('USD');
-    const [converted, setConverted] = useState('');
 
-
-    useEffect(() => {
-        if (fromCur === toCur) {
-            setConverted(amount)
-        }
-
-        fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`)
-            .then(response => response.json())
-            .then(value => setConverted(value?.rates[toCur]));
-    }, [amount, fromCur, toCur]);
 
     return (
         <main className={'layout'}>
-            <input type="text" value={amount}
-                   onChange={(e) => setAmount(Number(e.target.value))}/>
-
-            <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
-                <option value="USD">USD</option>
-                <option value="EUR">EUR</option>
-                <option value="CAD">CAD</option>
-                <option value="INR">INR</option>
-            </select>
-
-            <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
-                <option value="USD">USD</option>
-                <option value="EUR">EUR</option>
-                <option value="CAD">CAD</option>
-                <option value="INR">INR</option>
-            </select>
-
-            <p>{converted} {toCur}</p>
+            Layout
         </main>
     );
 };
