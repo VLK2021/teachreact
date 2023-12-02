3c0a61e7267121ce2b9891f2d93ba1c5cfbeb09b
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-12-03 00:54:03 +0200

    finished

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -5,51 +5,59 @@ import './LayoutStyle.css';
 
 const Layout = () => {
     const [countClick, setCountClick] = useState(0);
-    const [position, setPosition] =useState({});
+    const [position, setPosition] =useState({lat: null, lng: null });
+    const [error, setError] = useState(null);
 
 
     const {lat, lng} = position;
 
-//https://www.openstreetmap.org/#map=16/${lat}/${lang}
-
-
 
     const getPosition = () => {
-        setCountClick(prev => prev +1);
+        setCountClick((prev) => prev +1);
 
-        navigator.geolocation.getCurrentPosition(
-            (pos) => {
-                setPosition({
-                    lat: pos.coords.latitude,
-                    lng: pos.coords.longitude
-                })
-            }
-        )
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setPosition({
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    });
+                    setError(null);
+                },
+                (error) => {
+                    console.error('Error getting location:', error);
+                    setError('Error getting location. Please try again.');
+                }
+            );
+        } else {
+            console.error('Geolocation is not supported by this browser.');
+            setError('Geolocation is not supported by this browser.');
+        }
     }
-
 
 
     return (
         <main className={'layout'}>
             <div className={'layout-block-btn'}>
-                <button onClick={getPosition}>
-                    Get my location
-                </button>
+                <button onClick={getPosition}>Get my location</button>
             </div>
 
-            <p>You GPS position
-                <a
-                    href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
-                    target={'_blank'}
-                    rel={'noreferrer'}
-                >
-                    {lat} {lng}
-                </a>
-            </p>
+            {lat && lng && (
+                <p>
+                    Your GPS position{' '}
+                    <a
+                        href={`https://www.openstreetmap.org/#map=18/${lat}/${lng}`}
+                        target={'_blank'}
+                        rel={'noreferrer'}
+                    >
+                        {lat}, {lng}
+                    </a>
+                </p>
+            )}
 
-            <p>You requestted position {countClick} times</p>
+            {error && <p>{error}</p>}
 
-
+            <p>You requested position {countClick} times</p>
         </main>
     );
 };
