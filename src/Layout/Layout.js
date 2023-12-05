2ce1221be14cbf447de554093fdc03523e826ce8
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-12-05 23:50:14 +0200

    framer motion

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import { motion } from "framer-motion"
 
 import './LayoutStyle.css';
 
 
 
 const Layout = () => {
+    const items = ['Text1', 'Text2', 'Text3'];
+
+    const divVariants = {
+        visible: i => ({
+            opacity: 1,
+            transition: {
+                delay: i * 2,
+            }
+        }),
+        hidden: {opacity: 0}
+    }
 
 
     return (
-        <div className={'layout'}>
-            Layout
+        <div className={'layout flex-direction'}>
+            {
+                items.map((item, i) =>
+                    <motion.div
+                        key={item}
+                        variants={divVariants}
+                        initial={'hidden'}
+                        animate={'visible'}
+                        custom={i}
+                    >
+                        {item}
+                    </motion.div>)
+            }
         </div>
     );
 };
