2da5f6fb4fd7d99af7bc47079e4a3431b7a4111d
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-12-14 23:04:43 +0200

    start

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,32 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
 import './LayoutStyle.css';
 
 
 const Layout = () => {
-    const [fighters, setFighters] = useState();
 
-
-    useEffect(() => {
-        fetch('https://api.github.com/repos/binary-studio-academy/stage-2-es6-for-everyone/contents/resources/api/fighters.json')
-            .then(response => response.json())
-            .then(result => {
-                setFighters(JSON.parse(atob(result.content)));
-                console.log(JSON.parse(atob(result.content)))
-            })
-    }, []);
 
 
     return (
         <div className={'layout'}>
-            {
-                fighters &&
-                fighters.map((obj) =>
-                    <div key={obj.name}>
-                        <img src={obj.source} alt="foto"/>
-                    </div>
-                )
-            }
+            Layout
         </div>
     );
 };
